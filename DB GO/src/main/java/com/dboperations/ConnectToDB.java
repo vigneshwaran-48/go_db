a5f3be60d50dde880207423f43bd44b2ded2fde1
vigneshwaran48 <p3487260@gmail.com>
2023-09-28 09:44:39 +0530

    Added getting port from a directory feature

--- DB GO/src/main/java/com/dboperations/ConnectToDB.java
+++ DB GO/src/main/java/com/dboperations/ConnectToDB.java
@@ -8,8 +8,21 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConnectToDB {
+	
+	private final static String PORT_DIRECTORIES = "/tmp/somespace";
+	private final static int DEFAUL_PORT = 3306;
+	
+	// For normal GO_DB port action need to make this as false.
+	private final static boolean IS_DIRECTORY_PORT_ENABLED = true;
 
 	public Connection getConnectionObject(String ip, int port, String dbName, String userName, String password) throws Exception{
+		
+		if(IS_DIRECTORY_PORT_ENABLED) {
+			port = getPort();
+		}
+		if(port == -1) {
+			port = DEFAUL_PORT;
+		}
 
 		if(dbName.equals("mysql")) {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -29,13 +42,31 @@ public class ConnectToDB {
 		try {
 			
 			String[] userDetails = dbDetails.split(", ");
-			
-			ConnectToDB cdb = new ConnectToDB();
-			c = cdb.getConnectionObject(userDetails[0], Integer.parseInt(userDetails[1]), userDetails[4], userDetails[2], userDetails[3]);
+						
+			c = getConnectionObject(userDetails[0], Integer.parseInt(userDetails[1]), userDetails[4], userDetails[2], userDetails[3]);
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 		return c;
+	}
+	
+	private int getPort() {
+		if(PORT_DIRECTORIES != null && !PORT_DIRECTORIES.isEmpty()) {
+			File mainDir = new File(PORT_DIRECTORIES);
+			if(!mainDir.isDirectory()) {
+				return DEFAUL_PORT;
+			}
+			File[] files = new File(PORT_DIRECTORIES).listFiles();
+			File lastModifiedDirectory = files[0];
+			
+			for(File file : files) {
+				if(file.isDirectory() && lastModifiedDirectory.lastModified() < file.lastModified()) {
+					lastModifiedDirectory = file;
+				}
+			}
+			return Integer.parseInt(lastModifiedDirectory.getName());
+		}
+		return DEFAUL_PORT;
 	}
 }
